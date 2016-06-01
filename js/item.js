f3cfa4bd9fa4be5d09776d76bb2fc2e527ebe4e6
Benjamin Schug <benjaminschug@gmail.com>
2016-06-01 17:11:03 +0200

    made smallest font size a little larger to be more consistent with the game

--- js/item.js
+++ js/item.js
@@ -196,7 +196,7 @@ function Item (itemdata)
 	}
 
 	this.setFontSize = function (size) {
-		var actualSize = MathUtils.remap( size, 18, 45, 5, 24 );
+		var actualSize = MathUtils.remap( size, 18, 45, 8, 24 );
 		getLabel( this ).style.fontSize = (actualSize).toString() + 'px';
 	}
 
